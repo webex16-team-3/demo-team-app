e6d3d1e293e5cfdc9985ba54438916f80117b5e1
IK-ge <ig2267454@gmail.com>
2022-11-25 10:54:21 +0900

    テスト

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,4 @@
 <template>
   <h1>Home</h1>
-  <div>
-    <ul>
-      <li>
-        <a href="/quiz">Vueクイズ</a>
-      </li>
-      <li>
-        <a href="/palette">Vueパレット</a>
-      </li>
-      <li>
-        <a href="/memo">Vueメモ</a>
-      </li>
-    </ul>
-  </div>
+  <div></div>
 </template>
